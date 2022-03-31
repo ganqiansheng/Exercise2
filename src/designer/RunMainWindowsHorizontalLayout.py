7a5cf2de335b74f7ae8fac2510db1f2b47c5a658
ganqiansheng <96116440+ganqiansheng@users.noreply.github.com>
2022-03-31 21:06:04 +0800

    RunMainWindowswindowsHorizontal

--- src/designer/RunMainWindowsHorizontalLayout.py
+++ src/designer/RunMainWindowsHorizontalLayout.py
@@ -5,7 +5,7 @@ from  PyQt5.QtWidgets import QApplication,QMainWindow
 if __name__=='__main__':
     app=QApplication(sys.argv)
     mainWindow=QMainWindow()
-    uim=MainWindowsHorizontalLayout.Ui_MainWindow()
-    uim.setupUi(mainWindow)
+    ui=MainWindowsHorizontalLayout.Ui_MainWindow()
+    ui.setupUi(mainWindow)
     mainWindow.show()
     sys.exit(app.exec_())
