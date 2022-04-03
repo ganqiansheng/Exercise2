43ef647911eabeee69bb264188c76400771238ba
francesco <tosqh@163.com>
2022-04-03 19:43:06 +0800

    calendar

---
+++ src/calendar/MyCalendar.py
@@ -0,0 +1,69 @@
+
+
+import sys
+
+from PyQt5.QtCore import QMimeData, QDate,Qt
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+class MyCalendar(QDialog):
+    def __init__(self):
+        super(MyCalendar, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('日历演示案例')
+        self.resize(400,180)
+        self.calendar=QCalendarWidget(self)
+        self.calendar.setMinimumDate(QDate(1990,1,1))
+        self.calendar.setMaximumDate(QDate(2050,12,31))
+
+        self.calendar.setGridVisible(True)
+        self.calendar.move(20,20)
+
+        # palette=QPalette()
+        # palette.setColor(QPalette.Window, Qt.gray)
+        # palette.setColor(QPalette.window,Qt.lightGray)
+
+        layout1=QFormLayout()
+        layout2=QVBoxLayout()
+        # self.frame.setAutoFillBackground(True)
+        # self.frame.setPalette(palette)
+
+        self.btnGrid=QPushButton('隐藏网格线')
+        self.btnGrid.clicked.connect(self.ShowGrid)
+        layout2.addWidget(self.btnGrid)
+
+        self.btn2=QPushButton('其他功能键')
+        layout2.addWidget(self.btn2)
+        self.btnExit=QPushButton('退出')
+        self.btnExit.clicked.connect(self.QuitApp)
+        layout2.addWidget(self.btnExit)
+
+
+        layout1.addRow(self.calendar,layout2)
+
+
+        self.setLayout(layout1)
+
+    def ShowGrid(self):
+        # print(type(self))
+        if self.btnGrid.text()=='显示网格线':
+            self.btnGrid.setText('隐藏网格线')
+            self.calendar.setGridVisible(True)
+        else:
+            self.btnGrid.setText('显示网格线')
+            self.calendar.setGridVisible(False)
+
+    def QuitApp(self):
+        app=QApplication.instance()
+        app.quit()
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = MyCalendar()
+    main.show()
+
+    sys.exit(QApplication.exec_())
