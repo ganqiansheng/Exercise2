50211a3c492add52051476e78c4b061c4027bf57
francesco <tosqh@163.com>
2022-04-04 22:45:22 +0800

    DataLoction.py

---
+++ src/table_tree/DataLoction.py
@@ -0,0 +1,77 @@
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+class DataLoctionDemo(QWidget):
+    def __init__(self):
+        super(DataLoctionDemo, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('TableView演示案例')
+        self.resize(600,800)
+
+        layout=QVBoxLayout(self)
+        tableWidget=QTableWidget(self)
+        tableWidget.resize(550,750)
+        tableWidget.setRowCount(40)
+        tableWidget.setColumnCount(4)
+        tableWidget.setHorizontalHeaderLabels(['第1列','第2列','第3列','第4列'])
+        layout.addWidget(tableWidget)
+
+        for i in range(40):
+            for j in range(4):
+                tableWidgetItem='(%d,%d)'%(i,j)
+                tableWidget.setItem(i,j,QTableWidgetItem(tableWidgetItem))
+
+        self.setLayout(layout)
+
+        tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        text='(1'
+        items=tableWidget.findItems(text,Qt.MatchStartsWith)
+        print(items)
+        if len(items)>0:
+            for item in items:
+                item.setBackground(QBrush(QColor(0,255,0)))
+                item.setForeground((QBrush(QColor(255,0,0))))
+                row=item.row()
+                tableWidget.verticalScrollBar().setSliderPosition(row)
+
+
+
+
+        # tableWidget.resizeRowToContents(0)
+        # tableWidget.resizeColumnToContents(0)
+        # tableWidget.resizeColumnToContents(1)
+        # tableWidget.resizeColumnToContents(2)
+
+        # Custom = 2
+        # Fixed = 2
+        # Interactive = 0
+        # ResizeToContents = 3
+        # Stretch = 1
+
+        # tableWidget.horizontalHeader().setVisible(True)
+
+        # tableWidget.setShowGrid(False)
+
+
+
+
+
+    def tabelViewclicked(self,index):
+        print(type(index))
+
+        # print(self.tableView.index
+
+
+
+
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    main=DataLoctionDemo()
+    main.show()
+
+    sys.exit(app.exec_())
