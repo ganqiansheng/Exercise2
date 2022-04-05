fb422c23a1ced761ef5e4fc29a5d3960f52075f2
francesco <tosqh@163.com>
2022-04-05 23:06:23 +0800

    ScrollBar.py

--- src/containers/ScrollBar.py
+++ src/containers/ScrollBar.py
@@ -1,68 +1,63 @@
-import sys
+import sys,time
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtPrintSupport import *
 
 
-class ScrollBar(QMainWindow):
-    count = 0
-
+class ScrollsBarDemo(QWidget):
     def __init__(self):
-        super(ScrollBar, self).__init__()
-        self.initUI()
+        super(ScrollsBarDemo,self).__init__()
 
-    def initUI(self):
-        self.setWindowTitle('堆栈窗口控件演示案例')
-        self.resize(800, 600)
+        self.resize(300,400)
+        self.label=QLabel('显示当前时间')
 
-        mainLayout=QVBoxLayout()
-
-        self.label=QLabel('拖动滚动条来改变文字颜色')
+        mainLayout=QHBoxLayout()
+        self.label=QLabel('通过拖动滚动条来改变文字颜色')
         mainLayout.addWidget(self.label)
 
         layout=QHBoxLayout()
-
         self.scrollBar1 = QScrollBar()
-        self.scrollBar1.setMaximum(255)
-        self.scrollBar1.sliderMoved.connect(self.sliderMoved)
-
         self.scrollBar2 = QScrollBar()
-        self.scrollBar2.setMaximum(255)
-        self.scrollBar2.sliderMoved.connect(self.sliderMoved)
-
         self.scrollBar3 = QScrollBar()
-        self.scrollBar3.setMaximum(255)
-        self.scrollBar3.sliderMoved.connect(self.sliderMoved)
-
         self.scrollBar4 = QScrollBar()
-        self.scrollBar4.setMaximum(255)
-        self.scrollBar4.sliderMoved.connect(self.sliderMoved1)
+
+        self.scrollBar1.setMaximum(255)
+        self.scrollBar2.setMaximum(255)
+        self.scrollBar3.setMaximum(255)
+
+        self.scrollBar1.valueChanged.connect(self.changeColor)
+        self.scrollBar2.valueChanged.connect(self.changeColor)
+        self.scrollBar3.valueChanged.connect(self.changeColor)
+        self.scrollBar4.valueChanged.connect(self.moveLabel)
 
         layout.addWidget(self.scrollBar1)
         layout.addWidget(self.scrollBar2)
         layout.addWidget(self.scrollBar3)
         layout.addWidget(self.scrollBar4)
 
-
         mainLayout.addLayout(layout)
 
-        widget = QWidget()
-        self.setCentralWidget(widget)
-        widget.setLayout(mainLayout)
+        self.setLayout(mainLayout)
 
-    def sliderMoved(self):
+    def changeColor(self,value):
+        print(value)
         print(self.scrollBar1.value(),self.scrollBar2.value(),self.scrollBar3.value())
+        c=QColor(self.scrollBar1.value(),self.scrollBar2.value(),self.scrollBar3.value())
         palette=QPalette()
-        c=QColor(self.scrollBar1.value(),self.scrollBar2.value(),self.scrollBar3.value(),255)
         palette.setColor(QPalette.Foreground,c)
         self.label.setPalette(palette)
-    def sliderMoved1(self):
-        self.label.move(self.label.x(),self.y()+self.scrollBar4.value())
+
+    def moveLabel(self,value):
+        print(value)
+        print(self.label.x(),self.y(),value)
+        x=self.label.x()
+        y=self.label.y()
+        self.label.move(x,y+self.scrollBar4.value())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main = ScrollBar()
+    main = ScrollsBarDemo()
     main.show()
 
     sys.exit(app.exec_())
